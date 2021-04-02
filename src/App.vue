1723d3d8d20006d131eac565657432bb254324ad
Mete Uçar <71905780+Metecode@users.noreply.github.com>
2021-04-02 22:26:09 +0300

    ForgotPassword hazirlandi

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/home">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/ForgotPassword">ForgotPassword</router-link>
     </div>
     <router-view/>
   </div>
