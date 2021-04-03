304f59c5b9a4d304aed69a40976d04f8b3e5660f
Mete Uçar <71905780+Metecode@users.noreply.github.com>
2021-04-03 20:43:51 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/home">Home</router-link> |
-      <router-link to="/ForgotPassword">ForgotPassword</router-link>
+      <router-link to="/forgot-password">forgot-password</router-link>
     </div>
     <router-view/>
   </div>
