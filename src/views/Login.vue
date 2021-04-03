a37f312995ed80bedc0cb9f45e5f9d165a157628
bssenoz <bsenoz@hotmail.com>
2021-04-03 18:00:28 +0300

    login page eklendi

---
+++ src/views/Login.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="login">
+    <h1>Üye Girişi</h1>
+      <input
+        type="text"
+        id="username"
+        name="username"
+        v-model="input.username"
+        placeholder="Kullanıcı Adı"
+      />
+      <br>
+      <input
+        type="password"
+        id="password"
+        name="password"
+        v-model="input.password"
+        placeholder="Şifre"
+      />
+    <br /><br />
+    <button type="button" class="btn btn-secondary btn-lg" v-on:click="login()">
+      Giriş
+    </button>
+    <br /><br />
+    <p style="color: red; font-weight: bold">{{ alert }}</p>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {
+      authenticated: false,
+      alert: '',
+      input: {
+        username: '',
+        password: '',
+      },
+      users: [
+        {
+          username: 'deneme1',
+          password: '147258369',
+        },
+        {
+          username: 'deneme2',
+          password: '369258147',
+        },
+      ],
+    };
+  },
+  methods: {
+    login() {
+      if (this.input.username !== '' && this.input.password !== '') {
+        for (let i = 0; i < this.users.length; i += 1) {
+          if (
+            this.input.username === this.users[i].username
+            && this.input.password === this.users[i].password
+          ) {
+            this.authenticated = true;
+            this.$router.replace({ name: 'Home' });
+          } else {
+            this.alert = 'Kullanıcı adınızı veya şifrenizi hatalı girdiniz!';
+          }
+        }
+      } else {
+        this.alert = 'Lütfen kullanıcı adınızı ve şifrenizi giriniz!';
+      }
+    },
+  },
+};
+</script>
+
+<style>
+input,
+select {
+  width: 14%;
+  padding: 10px 18px;
+  margin: 6px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+</style>
