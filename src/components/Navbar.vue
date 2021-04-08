f5b2a15f87e33145a335d3bb7cc5a2b9f7750e6b
rumeysa8804 <72822875+rumeysa8804@users.noreply.github.com>
2021-04-08 23:25:15 +0300

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -111,24 +111,9 @@
 
 <script>
 export default {
-  props: ['Navbar'],
 };
 </script>
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-html,
-body {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
 .navbar {
   height: 60px;
   padding: 0;
