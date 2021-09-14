671639c067a64c84a00189fb6625baf65bbdb101
beyzaerkan <beyzaerkan@icloud.com>
2021-09-14 15:08:08 +0300

    navbar and footer improved

--- src/components/Layout/Footer.vue
+++ src/components/Layout/Footer.vue
@@ -1,72 +1,108 @@
 <template>
-  <footer class="footer text-center">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-6 col-md-4 col-sm-4 col-12 mt-3 mb-4 mb-md-0 x">
-          <div class="footer-brand">
-            <h5>ABOUT US</h5>
-            <a href="https://mavidurak.github.io/about" target="_blank">
+  <footer class="text-center text-lg-start bg-light text-muted">
+    <section class="container py-3">
+      <div class="text-center text-md-start mt-5">
+        <div class="row mt-3">
+          <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+            <h6 class="fw-bold mb-4 yazı">
               <img
-                src="https://pbs.twimg.com/profile_images/581806417012658176/jpcs3K-H_400x400.png"
-                alt="MaviDurakIO Logo"
-                width="80px"
-                height="80px"
+                src="../../assets/MaviDurak-IO.svg"
+                width="100"
+                height="100"
+                class="d-inline-block align-top logo"
+                alt=""
               />
-            </a>
+            </h6>
+            <p>
+              Find friends with similar interests, discussion and promotional
+              site for games, movies, TV shows and books.
+            </p>
+          </div>
+          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+            <h6 class="text-uppercase fw-bold mb-4">Products</h6>
+            <p>
+              <a href="#" class="text-reset">Node</a>
+            </p>
+            <p>
+              <a href="#" class="text-reset">Sequelize</a>
+            </p>
+            <p>
+              <a href="#" class="text-reset">Vue</a>
+            </p>
+            <p>
+              <a href="#" class="text-reset">PostgreSQL</a>
+            </p>
+          </div>
+          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+            <h6 class="text-uppercase fw-bold mb-4">Useful links</h6>
+            <p>
+              <a href="#" class="text-reset">Pricing</a>
+            </p>
+            <p>
+              <a href="#" class="text-reset">Settings</a>
+            </p>
+            <p>
+              <a href="#" class="text-reset">Orders</a>
+            </p>
+            <p>
+              <a href="#" class="text-reset">Help</a>
+            </p>
+          </div>
+          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+            <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+            <p><i class="fas fa-home me-3"></i> Sakarya, Türkiye</p>
+            <p>
+              <i class="fas fa-envelope me-3"></i>
+              mavidurak.io@gmail.com
+            </p>
+            <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
+            <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
           </div>
         </div>
-        <div class="col-lg-3 col-md-4 col-sm-4 col-6 mt-3 mb-4 mb-md-0 x">
-          <h6 class="text-uppercase">work with us</h6>
-          <ul class="list-unstyled mb-0">
-            <li><a href="#!" class="text-dark">Link 1</a></li>
-            <li><a href="#!" class="text-dark">Link 2</a></li>
-            <li><a href="#!" class="text-dark">Link 3</a></li>
-            <li><a href="#!" class="text-dark">Link 4</a></li>
-          </ul>
+      </div>
+    </section>
+    <div class="container text-center p-4">
+      <hr />
+      <div class="row">
+        <div class="col-md-8 col-sm-6 col-xs-12">
+          <p class="copyright-text">
+            Copyright &copy; 2021 All Rights Reserved by
+            <a href="https://mavidurak.github.io/" target="_blank"
+              >MaviDurak-IO</a
+            >.
+          </p>
         </div>
-        <div class="col-lg-3 col-md-4 col-sm-4 col-6 mt-3 mb-4 mb-md-0 x">
-          <h6 class="text-uppercase">company</h6>
-          <ul class="list-unstyled">
-            <li><a href="#!" class="text-dark">Link 1</a></li>
-            <li><a href="#!" class="text-dark">Link 2</a></li>
-            <li><a href="#!" class="text-dark">Link 3</a></li>
-            <li><a href="#!" class="text-dark">Link 4</a></li>
+
+        <div class="col-md-4 col-sm-6 col-xs-12">
+          <ul class="social-icons">
+            <li>
+              <a href="https://www.facebook.com/mavidurakio/" target="_blank"
+                ><i class="fab fa-facebook-f"></i
+              ></a>
+            </li>
+            <li>
+              <a href="https://twitter.com/mavidurakio" target="_blank"
+                ><i class="fab fa-twitter"></i
+              ></a>
+            </li>
+            <li>
+              <a href="https://www.instagram.com/mavidurak.io/" target="_blank"
+                ><i class="fab fa-instagram"></i
+              ></a>
+            </li>
+            <li>
+              <a href="https://t.me/mavidurakio" target="_blank"
+                ><i class="fab fa-telegram"></i
+              ></a>
+            </li>
+            <li>
+              <a href="https://discord.gg/7zaXka9" target="_blank"
+                ><i class="fab fa-discord"></i
+              ></a>
+            </li>
           </ul>
         </div>
       </div>
-    </div>
-    <div class="socials row">
-      <div class="col-lg-4 col-md-4 col-sm-0 emptyone"></div>
-      <ul class="iconlist list-unstyled col-lg-4 col-md-4 col-sm-6">
-        <li class="chosenone">
-          <a href="https://www.instagram.com/mavidurak.io" target="_blank"
-            ><i class="fab fa-instagram"></i
-          ></a>
-        </li>
-        <li>
-          <a href="https://twitter.com/mavidurakio" target="_blank"
-            ><i class="fab fa-twitter"></i
-          ></a>
-        </li>
-        <li>
-          <a href="https://github.com/mavidurak" target="_blank"
-            ><i class="fab fa-github"></i
-          ></a>
-        </li>
-        <li>
-          <a href="https://discord.gg/7zaXka9" target="_blank"
-            ><i class="fab fa-discord"></i
-          ></a>
-        </li>
-        <li>
-          <a href="https://t.me/mavidurakio" target="_blank"
-            ><i class="fab fa-telegram"></i
-          ></a>
-        </li>
-      </ul>
-      <span class="col-lg-4 col-md-4 col-sm-6 flex">
-        &#169; MaviDurak-IO 2021</span
-      >
     </div>
   </footer>
 </template>
@@ -78,90 +114,68 @@ export default {
 </script>
 <style lang="scss">
 footer {
-  position: relative;
-  left: 0;
-  bottom: 0;
-  width: 100%;
-  height: 150px;
-  background-color: #f9f8f4;
-  border-top: 1px solid rgb(212, 204, 204);
-
-  .container {
-    height: 150px;
-
-    .row {
-      height: 150px;
-    }
-  }
-
-  .row .x {
-    height: 130px;
-  }
-  .footer-brand {
-    height: 150px;
-
-    img {
-      border-radius: 100px;
-    }
-  }
-
-  .socials {
-    margin: 0;
-
-    span {
-      background-color: #1167b8;
-      text-align: right;
-      padding-right: 30px;
-      color: white;
-    }
-
-    .emptyone {
-      background-color: #1167b8;
-    }
-  }
-
-  .iconlist {
-    background-color: #1167b8;
-    padding: 2px;
+  .social-icons {
     text-align: center;
-    margin: 0;
-
-    li {
-      margin-left: 30px;
-      display: inline;
+  }
+  border: 1px solid #e7ebf0;
+}
+.social-icons {
+  padding-left: 0;
+  margin-bottom: 0;
+  list-style: none;
+  li {
+    display: inline-block;
+    margin-bottom: 4px;
+  }
+  li.title {
+    margin-right: 15px;
+    text-transform: uppercase;
+    color: #144585;
+    font-weight: 700;
+    font-size: 13px;
+  }
+  a {
+    background-color: #eceeef;
+    color: #818a91;
+    font-size: 16px;
+    display: inline-block;
+    line-height: 44px;
+    width: 44px;
+    height: 44px;
+    text-align: center;
+    margin-right: 8px;
+    border-radius: 100%;
+    -webkit-transition: all 0.2s linear;
+    -o-transition: all 0.2s linear;
+    transition: all 0.2s linear;
+    &:active {
+      color: #fff;
+      background-color: rgb(208, 215, 221);
     }
-    .chosenone {
-      margin-left: 0;
+    &:focus {
+      color: #fff;
+      background-color: rgb(208, 215, 221);
+    }
+    &:hover {
+      color: #fff;
+      background-color: rgb(208, 215, 221);
     }
   }
-
-  .iconlist li i {
-    font-size: 18px;
-    color: #fff;
-  }
-
-  .copyright {
-    background-color: rgb(41, 102, 126);
-    color: white;
+}
+.social-icons.size-sm {
+  a {
+    line-height: 34px;
+    height: 34px;
+    width: 34px;
     font-size: 14px;
   }
-
-  @media (max-width: 576px) {
-    .footer {
-      height: 350px;
-    }
-    .container {
-      height: 350px;
-    }
-
-    .socials {
-      .iconlist {
-        text-align: center;
-      }
-      span {
-        text-align: center;
-        padding-right: 0;
-      }
+}
+@media (max-width: 767px) {
+  .social-icons {
+    li.title {
+      display: block;
+      margin-right: 0;
+      font-weight: 600;
     }
   }
 }
